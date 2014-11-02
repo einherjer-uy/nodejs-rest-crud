02562500186c6283011eab52686e197f91a17714
Andres Sica <andres.sica@gmail.com>
2014-11-01 23:11:10 -0200

    added mongoose toJSJSON.transform

--- app/models/scratchcode.js
+++ app/models/scratchcode.js
@@ -19,6 +19,14 @@ ScratchcodesSchema.methods.reset = function (callback) {
   this.available = true;
 }
 
+if (!ScratchcodesSchema.options.toJSON) {
+	ScratchcodesSchema.options.toJSON = {};
+}
+ScratchcodesSchema.options.toJSON.transform = function (doc, ret, options) {
+	delete ret._id;
+	delete ret.__v;
+}
+
 ScratchcodesSchema.index({ code: 1 });
 
 module.exports = mongoose.model('Scratchcode', ScratchcodesSchema);
